3fc10cec5820435b3867985da3eea86c2bb65808
harshil6-lab <harshilkalsariya6@gmail.com>
2025-04-10 21:48:28 +0530

    1.2.cpp

--- 1.2.cpp
+++ 1.2.cpp
@@ -10,75 +10,93 @@ class inventry {
 
 public:
     void add_products();
-    void update_product();
-    void total_value_of_product();
-    void display_product_details();
-       
+    void update_product(inventry pro[], int size);
+    void total_value_of_product(inventry pro[], int size);
+    void display_product_details(inventry pro[], int size);
+
+    int getId() { return id; }
+    string getName() { return name; }
+    float getQuantity() { return quantity; }
+    float getPrice() { return price; }
+
+    void setQuantity(float q) { quantity = q; }
 };
-inventry pro[100];
+
 int j = 0;
 
-void inventry :: add_products() {
-    cout << "Enter Name of Product : " << endl;
+void inventry::add_products() {
+    cout << "Enter Name of Product : ";
     cin >> name;
-    cout << "Enter ID Of Product : " << endl;
+    cout << "Enter ID Of Product : ";
     cin >> id;
-    cout << "Enter Quantity Of Product : " << endl;
+    cout << "Enter Quantity Of Product : ";
     cin >> quantity;
     cout << "Enter Price of Product  :  " << endl;
     cin >> price;
 }
 
-void inventry :: update_product() {
+void inventry::update_product(inventry pro[], int size) {
     int idp;
     cout << "Enter ID of Product : ";
     cin >> idp;
-    for (int j = 0; j < 100; j++) {
-        if (pro[j].id == idp) {
+    int a=0;
+    for (int k = 0; k < size; k++) {
+        if (pro[k].getId() == idp) {
+            float newQty;
             cout << "Update A Quantity : ";
-            cin >> pro[j].quantity;
+            cin >> newQty;
+            pro[k].setQuantity(newQty);
             cout << "Quantity Updated Successfully!" << endl;
+            a =1;
+            break;
         }
     }
-    cout << "Product ID not found!" << endl;
+    if (!a)
+        cout << "Product ID not found!" << endl;
 }
 
-void inventry :: total_value_of_product() {
-        int idn;
-        float total[j];
-        cout << "Enter Product Id: ";
-        cin >> idn;
-        for (int j = 0; j < 100 ; j++) {
-            if (pro[j].id == idn) {
-                total[j] = (pro[j].quantity * pro[j].price);
-                cout << "Product Total Value Is : " << total[j] << endl;
-            }
+void inventry::total_value_of_product(inventry pro[], int size) {
+    int idn;
+    cout << "Enter Product Id: ";
+    cin >> idn;
+    int a=0;
+    for (int k = 0; k < size; k++) {
+        if (pro[k].getId() == idn) {
+            float total = pro[k].getQuantity() * pro[k].getPrice();
+            cout << "Product Total Value Is : " << total << endl;
+            a = 1;
+            break;
         }
-        cout << "Product ID not found!" << endl;
     }
-    
-    void inventry :: display_product_details() {
-        int ids;
-        cout << "Enter Id Of Product : ";
-        cin >> ids;
-        for (int j = 0; j < 100; j++) {
-            if (pro[j].id == ids) {
-                cout << "Name : " << pro[j].name << endl;
-                cout << "ID   : " << pro[j].id << endl;
-                cout << "Price: " << pro[j].price << endl;
-                cout << "Total Value : " << (pro[j].quantity * pro[j].price) << endl << endl;
-                cout << "Thank You !!" << endl;
-            }
-        }
+    if (!a)
         cout << "Product ID not found!" << endl;
-    }
+}
 
+void inventry::display_product_details(inventry pro[], int size) {
+    int ids;
+    cout << "Enter Id Of Product : ";
+    cin >> ids;
+    int a=0;
+    for (int k = 0; k < size; k++) {
+        if (pro[k].getId() == ids) {
+            cout << "Name : " << pro[k].getName() << endl;
+            cout << "ID   : " << pro[k].getId() << endl;
+            cout << "Price: " << pro[k].getPrice() << endl;
+            cout << "Total Value : " << (pro[k].getQuantity() * pro[k].getPrice()) << endl << endl;
+            cout << "Thank You !!" << endl;
+            a=1;
+            break;
+        }
+    }
+    if (!a)
+        cout << "Product ID not found!" << endl;
+}
 
 int main() {
     int choice;
-    
+    inventry pro[100];
 
-    cout << "Follow This Number System : \n";
+    cout << "\nFollow This Number System : \n";
     cout << "1 for Add a Product \n";
     cout << "2 for Update Quantity \n";
     cout << "3 for Total Value Of Product \n";
@@ -86,7 +104,7 @@ int main() {
     cout << "0 for Exit \n";
 
     do {
-        cout << "Enter Your Choice : ";
+        cout << "\nEnter Your Choice : ";
         cin >> choice;
 
         switch (choice) {
@@ -96,15 +114,15 @@ int main() {
                 cout << "Thank You !!" << endl;
                 break;
             case 2:
-                pro[j].update_product();
+                pro[0].update_product(pro, j);
                 cout << "Thank you !!" << endl;
                 break;
             case 3:
-                pro[j].total_value_of_product();
+                pro[0].total_value_of_product(pro, j);
                 cout << "Thank You !!" << endl;
                 break;
             case 4:
-                pro[j].display_product_details();
+                pro[0].display_product_details(pro, j);
                 cout << "Thank You !!" << endl;
                 break;
             case 0:
@@ -114,6 +132,7 @@ int main() {
                 cout << "ERROR :::: Enter Numbers In Range of (0 - 4) !!" << endl;
         }
     } while (choice != 0);
-    cout<<"24CE049_Harshil"<<endl;
+
+    cout << "24CE049_Harshil" << endl;
     return 0;
 }
