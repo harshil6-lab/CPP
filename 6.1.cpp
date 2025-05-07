56924c95dda9a1d4176e24ac7ad63779c260f585
Harshil kalsariya <harshilkalsariya6@gmail.com>
2025-05-07 16:02:33 +0530

    Add files via upload

---
+++ 6.1.cpp
@@ -0,0 +1,330 @@
+Faculty of Technology and Engineering
+U & P U. Patel Department of Computer Engineering
+
+Date: 01 / 07 / 2024
+
+Academic Year : 2024-2025 Semester : 1
+Course code : CEUC101 Course name : CCP
+Sr.
+No.
+
+Practical Definition Hrs CO
+Basic create and run C program
+
+1. Write and execute a C program to print your name on the screen using various text
+editors (like Notepad, Word in Windows, vi, gedit in Linux) and IDEs (like
+TurboC++, Code::Blocks, VS Code) in both Linux and Windows environments.
+Check the additional files generated after compiling and running the C program.
+Demonstrate the use of the preprocessor, compiler, and assembler in converting the
+C program to executable code.
+
+2
+
+Data Types, Variables, Input, Output
+
+2. The Department of Urban Planning needs to calculate the area and perimeter of a
+rectangular plot of land for planning purposes. Given the length of 50 meters and
+the breadth of 30 meters, write a program to calculate the area and the perimeter
+and display the results.
+
+0.5
+
+3. State Bank of India wants to calculate the simple interest on savings accounts. The
+bank offers a principal amount of ₹50,000 at an annual interest rate of 5.5% for a
+time period of 3 years. Write a program to calculate and display the simple interest.
+0.5
+
+4. Home Ministry of Bharat want to calculate the count illiterate men and women,
+Bharat population for year 2024 is 1,441,981,744. Out of them 48.4% are women.
+Total literacy rate is 85.95%. Literacy rate of male population is 80.95% and
+62.84% for female population. Write a program to get desired output for Home
+Ministry.
+
+0.5
+
+5. The Meteorological Department of India needs to convert temperature readings
+from Celsius to Fahrenheit for their weather reports. Given a temperature in
+Celsius, write a program to convert it to Fahrenheit using the given formula and
+display the result.
+Fahrenheit = (Celsius ×
+9
+5
+) + 32
+
+0.5
+
+6. The Finance Department needs a simple calculator program to perform basic
+arithmetic operations for internal calculations. Given two fixed numbers write a
+program to perform addition, subtraction, multiplication, and division based on
+entered signs +, -, *, / respectively and display the appropriate result.
+
+0.5
+
+Branch Statements
+
+By : TRUSHA PATEL, AAYUSHI CHAUDHARI
+7. Raman and Suman, siblings residing in different cities in Karnataka, each own
+multiple properties. Raman's assets include a bungalow valued at ₹12,000,000, a
+plot valued at ₹6,000,000, and a car valued at ₹3,000,000. Meanwhile, Suman owns
+an apartment valued at ₹11,000,000, a hotel valued at ₹8,000,000, and a car valued
+at ₹8,000,000. Determine who possesses the greater total wealth between them or
+whether they are equally wealthier.
+
+0.5
+
+8. Foodies is a restaurant established in 2020. Due to rush of customers, the waiters
+are not able to manage the food supply appropriately. To help them, create a menu
+ordering system, which allows customers to select the items from the menu and
+compute the total cost of their order. Use switch case for menu ordering and item
+selection.
+Menu includes the following:
+ Burger - ₹150
+ Pizza - ₹200
+ Pasta - ₹120
+ Sandwich - ₹100
+ French Fries - ₹80
+Display the menu to user and allow them to enter the number they wish to order.
+Calculate the total amount after selection of all items. Ask user to enter ‘0’ after
+finishing the ordering of items.
+
+1
+
+9. Determine the grade of a student based on their marks using the conditional
+(ternary) operator. Take student’s marks as input and display the corresponding
+grade as output according to the following criteria:
+ Marks >= 90: Grade A
+ Marks >= 80 and < 90: Grade B
+ Marks >= 70 and < 80: Grade C
+ Marks >= 60 and < 70: Grade D
+ Marks < 60: Grade F
+Validate the input by ensuring user is entering marks between 0-100, else declare
+that the entered input is invalid.
+
+1
+
+Looping Statements
+
+10. Develop a countdown timer, that allows user to set a starting number of seconds
+and then count down to zero, displaying each second as it decrements. After the
+countdown completes write ‘Countdown completed!’.
+
+0.5
+
+11. Develop a C program that simulates a matchstick game between the user and the
+computer. The objective of the game is to avoid picking the last matchstick. The
+program should ensure that the computer always wins by strategically picking
+matchsticks. The game starts with 21 matchsticks. The user and the computer take
+turns to pick 1, 2, 3, or 4 matchsticks. The player forced to pick the last matchstick
+loses the game.
+Rules:
+1. The game starts with 21 matchsticks.
+2. The user is asked to pick 1, 2, 3, or 4 matchsticks.
+3. After the user picks, the computer makes its pick.
+4. The player who is forced to pick the last matchstick loses the game
+To understand the above game in a better way, visit the following link:
+http://atozmath.com/Games/21MatchStick.aspx
+
+1
+
+By : TRUSHA PATEL, AAYUSHI CHAUDHARI
+12. A company organizes events on even days of the month, from day 1 to day 100.
+The management wants to calculate the total budget allocated for these events.
+Each even day costs 200 Rs. Write a C program to compute the total budget
+allocated for all even-day events.
+
+0.5
+
+13. Write a C program to generate and display a multiplication table based on user
+input in the following form:
+
+1
+
+14. Use appropriate nested loops to draw the following patterns: 2
+(i)
+1
+1 0
+1 0 1
+1 0 1 0
+1 0 1 0 1
+(ii)
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+(iii)
+
+A
+A B A
+A B C B A
+A B C D C B A
+
+Arrays
+
+15. Consider a situation where a user needs to analyze a set of numbers they input into
+a system. Imagine a data entry operator who inputs 25 different numbers via the
+keyboard. The goal is to develop a C program that processes these numbers to
+determine specific characteristics: how many of the numbers are positive, how
+many are negative, how many are even, and how many are odd.
+
+0.5
+
+16. Store manager needs to sort the prices of items in inventory to prepare a report.
+The prices are initially listed in an unordered manner, and sorting them in
+ascending order will facilitate easier analysis and decision-making regarding
+pricing strategies and stock management.
+Hint: Let the user decide the number of items
+
+1
+
+17. Display the seating arrangement in theatre using C program. The theater has a fixed
+number of rows and seats per row. Create a seating chart where each seat is
+identified by its row and seat number. Additionally, the program should allow the
+user to mark certain seats as reserved. The seating chart should be displayed with
+indicators showing which seats are reserved and which are available.
+Expected Outcome:
+
+2
+
+By : TRUSHA PATEL, AAYUSHI CHAUDHARI
+18. Let us assume, teacher is supposed to allot seats based on the student’s names. You
+are requested to help teacher by creating a C program, for collecting the names of 5
+students and sort them in alphabetical order.
+
+1
+
+19. Develop a C program to manage a simple text-based note-taking application. Users
+should be allowed to perform various string operations on their notes without using
+functions from the <string.h> header file. The operations include calculating the
+length of a note, reversing a note, comparing two notes, copying a note, and
+concatenating two notes.
+
+2
+
+User Defined Functions
+
+20. Create a simple student grading system, which should include various functions to
+handle different tasks related to the management and processing of student grades.
+The functionalities should demonstrate the use of functions with different
+combinations of arguments and return values as specified below:
+1. No arguments passed and no return value: Display a welcome to
+CHARUSAT message.
+2. No arguments passed but a return value: Collect the number of students
+based on user input.
+3. Argument passed but no return value: Collect and display the grades of
+students based on the count of students collected in previous function.
+4. Argument passed and a return value: Calculate the average grade of the
+students by collecting the same grades and display their average.
+Expected Outcome:
+
+2
+
+21. Verify whether three given lengths can form a triangle. If they do, the program
+should then calculate the area of the triangle using Heron's formula. The program
+should use nested functions to accomplish this. Specifically, the program should:
+1
+
+By : TRUSHA PATEL, AAYUSHI CHAUDHARI
+
+1. Collect the lengths of the three sides of the triangle from the user.
+2. Verify whether the given lengths can form a triangle.
+3. If the lengths form a valid triangle, calculate the area of the triangle using
+Heron's formula.
+4. Display the result to the user.
+
+22. Imagine you are tasked with writing a program to help a teacher analyse student
+performance in CCP subject. The program will accept a list of student marks and
+perform the following tasks:
+1. Calculate Total Marks: Write a function that takes the array of marks
+and returns the total marks of all students.
+2. Calculate Average Marks: Write a function that takes the array of marks
+and returns the average marks.
+3. Find Highest Marks: Write a function that takes the array of marks and
+returns the highest marks.
+4. Find Lowest Marks: Write a function that takes the array of marks and
+returns the lowest marks.
+5. Display the results to the user.
+Expected outcome:
+
+2
+
+23. Imagine you are working on a program that predicts how something grows over
+time, like how much money you might save each month. The growth pattern
+follows a rule similar to the Fibonacci series, where the amount saved each month
+is the sum of the two previous months. To help calculate this, you need a program
+that can generate the Fibonacci series. Take the number of series user want to see
+through keyboard input.
+
+1.5
+
+Structure and Union
+
+24. Write a C program to create a structure of Book Detail and display the details of the
+book in appropriate format by passing structure as a function argument.
+Book Detail must contain following information:
+Book Title, Author name and Amount of book in float.
+
+1
+
+25. Create a Union called library to hold accession number, title of the book, author name,
+price of the book and flag indicating whether the book is issued or not. (flag = 1 if the
+1
+
+By : TRUSHA PATEL, AAYUSHI CHAUDHARI
+
+book is issued, flag = 0 otherwise). Write a program to enter data of one book and
+display the data.
+26. You are tasked with creating a system for managing sports teams in
+CHARUSAT. Each team consists of a team name, sport type (e.g., basketball,
+football), and a coach. Each coach has details such as name, age, and experience.
+Your program should:
+ Store information about multiple sports teams and their coaches.
+ Allow the user to add, search, and display team and coach information.
+Hint: Use a nested structure where the team structure contains another structure
+for coach details.
+
+1.5
+
+Pointers and Dynamic Memory Allocation
+
+27. Imagine a situation where two values need to be swapped within a banking system,
+such as updating balances between two accounts after a transaction. Make use of
+pointer as function arguments for swapping function.
+
+1
+
+28. Write a program to read the marks of 10 students for the subject CEUC101
+Computer concepts and Programming and computes the number of students in
+categories FAIL, PASS, FIRST
+CLASS and DISTINCTION using Pointers and Arrays.
+Marks Categories
+70 or above DISTINCTION
+69 to 60 FIRST CLASS
+59 to 40 PASS
+Below 40 FAIL
+For example, if following marks of 10 students are entered:
+34 56 78 98 12 31 67 75 91 23
+Then the output should be
+DISTINCTION 4 FIRST CLASS 1 PASS 1 FAIL 4
+
+1.5
+
+29. Write a C program using a character string in a block of memory space created by
+calloc () and then modify the same to store a larger string using realloc () function.
+(Dynamic Array).
+
+1.5
+
+30. a. Write a C program to read a text file ‘Demo.txt’ and print each word of that file
+in reverse order.
+Example:
+Input: HELLO
+Output: OLLEH
+b. You are building a simple student marks recording system that:
+1. Writes students' marks to a file.
+2. Reads the marks from the file.
+The program will use:
+ putw() to write the marks (integer values) into the file.
+ getw() to read the marks from the file.
+ fopen() and fclose() to open and close the file
